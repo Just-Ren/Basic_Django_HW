31015b831a3a84dbb546a7f8392d9b535f915add
Eugene <evgenii.evgenii.02@mail.ru>
2024-11-09 23:07:49 +0300

    домашнее задание по блоку 23-работа с ORM в Django

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,14 +2,13 @@ from django.urls import path
 
 from catalog.apps import CatalogConfig
 
-from .views import (ContactView, ProductListView, ProductCreateView, ProductDeleteView, ProductDetailView,
-                    ProductUpdateView)
+from .views import ContactView, ProductListView, ProductCreateView, ProductDeleteView, ProductDetailView, ProductUpdateView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('products/', ProductListView.as_view(), name='products_list'),
-    path('products/<int:pk>/create/', ProductCreateView.as_view(), name='product_create'),
+    path('products/create/', ProductCreateView.as_view(), name='product_create'),
     path('products/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('products/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
